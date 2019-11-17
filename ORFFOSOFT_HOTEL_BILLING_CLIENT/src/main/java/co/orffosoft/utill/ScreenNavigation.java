99633d45e4ee3befc5cb8ed5c4a8a3357b5a28ec
Mithun <Mithun@VAIO>
2019-11-17 10:41:13 -0800

    Initial commit

---
+++ ORFFOSOFT_HOTEL_BILLING_CLIENT/src/main/java/co/orffosoft/utill/ScreenNavigation.java
@@ -0,0 +1,10 @@
+package co.orffosoft.utill;
+
+import lombok.Data;
+
+@Data
+public class ScreenNavigation {
+	public static final String STOCK_UTILITY_ADD = "/pages/Orrfo_Billing_Pages/Stock_Utility/StockUtilityAdd?faces-redirect=true;";
+
+
+}
